c1a186df43d2b883fb07e3e9a81114e4895f0ab6
Rakesh <rakesh@Rakeshs-Mac.local>
2013-02-18 20:23:53 +0530

    Initial Commit

---
+++ Dabble/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Dabble
+//
+//  Created by Rakesh on 18/02/13.
+//  Copyright (c) 2013 Rakesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Dabble/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Dabble
+//
+//  Created by Rakesh on 18/02/13.
+//  Copyright (c) 2013 Rakesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
