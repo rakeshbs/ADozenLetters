8b6117b7bc81c63651f2ff61bf3d250f20f21f74
Rakesh <rakeshbs@gmail.com>
2013-05-10 15:21:30 +0530

    game centre

---
+++ Dabble/Game Logic/DabbleGCHelper.h
@@ -0,0 +1,13 @@
+//
+//  DabbleGCHelper.h
+//  Dabble
+//
+//  Created by Rakesh on 07/05/13.
+//  Copyright (c) 2013 Rakesh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DabbleGCHelper : NSObject
+
+@end
---
+++ Dabble/Game Logic/DabbleGCHelper.m
@@ -0,0 +1,13 @@
+//
+//  DabbleGCHelper.m
+//  Dabble
+//
+//  Created by Rakesh on 07/05/13.
+//  Copyright (c) 2013 Rakesh. All rights reserved.
+//
+
+#import "DabbleGCHelper.h"
+
+@implementation DabbleGCHelper
+
+@end
