63645a4ef4a6f3c2c34fec9b85896c77d0bdf2e4
Rakesh <rakeshbs@gmail.com>
2013-05-24 16:10:14 +0530

    GC

---
+++ Dabble/Game Logic/Game.h
@@ -0,0 +1,13 @@
+//
+//  Game.h
+//  Dabble
+//
+//  Created by Rakesh on 24/05/13.
+//  Copyright (c) 2013 Rakesh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Game : NSObject
+
+@end
---
+++ Dabble/Game Logic/Round.h
@@ -0,0 +1,13 @@
+//
+//  Round.h
+//  Dabble
+//
+//  Created by Rakesh on 24/05/13.
+//  Copyright (c) 2013 Rakesh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Round : NSObject
+
+@end
